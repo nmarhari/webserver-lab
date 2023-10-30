e477f6e97fb936e320faebf5b59442516cf981e9
nmarhari <nassimmarhari@gmail.com>
2023-10-30 15:43:13 -0400

    blank white page

--- webserver.py
+++ webserver.py
@@ -5,7 +5,7 @@ import sys
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a server socket
-serverSocket.bind(("localhost", 12345))
+serverSocket.bind(("192.168.1.121", 12345))
 serverSocket.listen(1)
 
 while True:
@@ -13,13 +13,13 @@ while True:
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     try:
-        message = "HelloWorld.html"
+        message = connectionSocket.recv(1024)
         filename = message.split()[1]
         f = open(filename[1:])
         outputdata = f.read()
         #Send one HTTP header line into socket
-        serverSocket.send("HTTP/1.1 200 OK")
-        serverSocket.send("\r\n")
+        connectionSocket.send("HTTP/1.1 200 OK".encode())
+        connectionSocket.send("\r\n\r\n".encode())
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send("\r\n".encode())
@@ -28,8 +28,8 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        error = "Error! File not found."
-        connectionSocket.send(error)
+        connectionSocket.send("HTTP/1.1 404 Not Found".encode())
+        connectionSocket.send("\r\n\r\n".encode())
         #Close client socket
         connectionSocket.close()
 
