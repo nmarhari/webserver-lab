5c01f4026c6537c610ddc8574627e7d6811e840b
nmarhari <nassimmarhari@gmail.com>
2023-11-02 22:53:04 -0400

    comments

--- webserver.py
+++ webserver.py
@@ -6,6 +6,7 @@ import threading
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a server socket
+#Main thread that listens for clients at a fixed port (12345)
 serverSocket.bind(("192.168.1.121", 12345))
 serverSocket.listen(1)
 
@@ -14,12 +15,13 @@ def start():
         #Establish the connection
         print('Ready to serve...')
         connectionSocket, addr = serverSocket.accept()
+        #When it receives a TCP connection, set up TCP connection
+        #through another port in a separate thread
         thread = threading.Thread(target=connect, args=(connectionSocket, addr))
         thread.start()
         
-
 def connect(connectionSocket, addr):
-    print('Active Connections: ', (threading.active_count() - 1))
+    #Show which port the TCP connection is on
     print('Connected from: ', addr)
     while True:
         try:
@@ -43,9 +45,7 @@ def connect(connectionSocket, addr):
             connectionSocket.close()
             break
             
-        
 start()
-
 
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
