2025ea3131683f338907072fe132f29c0b236d37
nmarhari <nassimmarhari@gmail.com>
2023-11-03 14:37:25 -0400

    works w/ argument and change filename

---
+++ client.py
@@ -0,0 +1,22 @@
+#Nassim Marhari
+from socket import *
+import sys
+
+HEADER = 64
+FORMAT = 'UTF-8'
+
+client = socket(AF_INET, SOCK_STREAM)
+
+
+def send(argv):
+    client.connect((sys.argv[1], int(sys.argv[2])))
+    msg = ("GET /")
+    msg += sys.argv[3]
+    msg += (" HTTP/1.1\r\nHost:192.168.1.121\r\nConnection: close\r\n\r\n")
+    msg = msg.encode()
+    client.send(bytes(msg))
+    response = client.recv(4096)
+    print(response.decode())
+
+
+send(sys.argv)
